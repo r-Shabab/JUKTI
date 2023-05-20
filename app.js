c525fab081bfbf31765bcdd33bd3af4c3233a6ae
r-shabab <raashidshabab@gmail.com>
2023-05-20 21:56:56 +0600

    added navbar (responsive)

---
+++ app.js
@@ -0,0 +1,29 @@
+// SHOW MENU
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Menu show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// MENU HIDDEN
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// Remove menu
+
+const navLink = document.querySelectorAll(".navbar_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
